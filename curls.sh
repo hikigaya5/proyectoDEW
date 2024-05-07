d2a4ca46ea8cf82beced2c50f5ebe16ceb132390
celiagarciam <132065179+celiagarciam@users.noreply.github.com>
2024-05-07 16:57:04 +0200

    Update curls.sh

--- curls.sh
+++ curls.sh
@@ -1,10 +1,14 @@
-KEY=$(curl -s --data '{"dni":"23456733H","password":"123456"}'-X POST -H "content-type: application/json" http://dew-cgarmon1-2324.dsicv.upv.es:9090/CentroEducativo/login -c cucu -b cucu)
 
-curl -s -X GET 'http://dew-cgarmon1-2324.dsicv.upv.es:9090/CentroEducativo/alumnos?key='$KEY -H "accept: application/json" -c cucu -b cucu
+KEY=$(curl -s --data '{"dni":"23456733H","password":"123456"}' -X POST -H "content-type: application/json" http://dew-cgarmon1-2324.dsicv.upv.es:9090/CentroEducativo/login -c cucu -b cucu)
 
-curl -s  --data '{“apellidos”:”Fernándex”, "dni":"222222222H",”nombre”:”Maria”, "password":"123456"}' -X POST -H”content-type: application/json” http://dew cgarmon1-2324.dsicv.upv.es:9090/CentroEducativo/alumnos?key='$KEY -c cucu -b cucu)
 
- curl -s --data '{"dni":"222222222H"}'-X GET 'http://dew-cgarmon1-2324.dsicv.upv.es:9090/CentroEducativo/alumnos/?key='$KEY -H "accept: application/json" -c cucu -b cucu
+curl -s -X GET "http://dew-cgarmon1-2324.dsicv.upv.es:9090/CentroEducativo/alumnos?key=$KEY" -H "accept: application/json" -c cucu -b cucu
+
+
+curl -s --data '{"apellidos":"Fernández", "dni":"222222222H", "nombre":"Maria", "password":"123456"}' -X POST -H "content-type: application/json" "http://dew-cgarmon1-2324.dsicv.upv.es:9090/CentroEducativo/alumnos?key=$KEY" -c cucu -b cucu
+
+
+curl -s --data '{"dni":"222222222H"}' -X GET "http://dew-cgarmon1-2324.dsicv.upv.es:9090/CentroEducativo/alumnos/?key=$KEY" -H "accept: application/json" -c cucu -b cucu
 
 
 
